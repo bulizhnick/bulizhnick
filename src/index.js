ab0d45bc087ca0b5465d411e8ca9b81655b5f296
Vladislav Korbut <v.korbut8@gmail.com>
2018-10-28 13:55:32 +0300

    Update import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import { store } from './config/redux';
-import { App } from './components/App/app.component';
+import { App } from './components/App/AppContainer';
 
 /* eslint-disable react/jsx-filename-extension */
 ReactDOM.render(
